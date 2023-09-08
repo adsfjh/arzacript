966c9b6e4f3d6597defbb07d6e2892824bb5a426
datealivezz <103813231+datealivezz@users.noreply.github.com>
2023-09-08 18:26:59 +0700

    Update forum.js

--- forum.js
+++ forum.js
@@ -3,7 +3,7 @@
 // @namespace   arzmesa
 // @match       https://forum.arizona-rp.com/*
 // @grant       none
-// @version     10
+// @version     11
 // @author      1clyde
 // @description teh
 // ==/UserScript==
