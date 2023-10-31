da05d830c18f810e3d6d95fca41cd7a6b3bb6786
1clyde <73130553+adsfjh@users.noreply.github.com>
2023-10-31 21:18:09 +0300

    Update forum.js

--- forum.js
+++ forum.js
@@ -3,7 +3,7 @@
 // @namespace   arzmesa
 // @match       https://forum.arizona-rp.com/*
 // @grant       none
-// @version     13
+// @version     14
 // @author      1clyde
 // @description teh
 // ==/UserScript==
